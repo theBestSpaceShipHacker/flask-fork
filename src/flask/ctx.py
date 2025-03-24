0c568fb24417068a62736cee7d85d977e7bc273e
theBestSpaceShipHacker <69464254+theBestSpaceShipHacker@users.noreply.github.com>
2025-03-24 08:31:56 +0000

    Pass the request ctx rather than use the globals in the app The globals have a performance penalty which can be justified for the convinience in user code. In the app however the ctx can easily be passed through the method calls thereby reducing the performance penalty.
    This may affect extensions if they have subclassed the app and
    overridden these methods.

--- src/flask/ctx.py
+++ src/flask/ctx.py
@@ -188,7 +188,8 @@ def copy_current_request_context(f: F) -> F:
 
     def wrapper(*args: t.Any, **kwargs: t.Any) -> t.Any:
         with ctx:  # type: ignore[union-attr]
-            return ctx.app.ensure_sync(f)(*args, **kwargs)  # type: ignore[union-attr]
+            # type: ignore[union-attr]
+            return ctx.app.ensure_sync(f)(*args, **kwargs)
 
     return update_wrapper(wrapper, f)  # type: ignore[return-value]
 
